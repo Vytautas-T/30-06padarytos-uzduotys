63655c8c89bc476989140180aa3e250d9a980c6d
Vytautas-T <105976888+Vytautas-T@users.noreply.github.com>
2022-06-30 20:56:02 +0300

    ketvirta uzduotis pabaikta

--- src/ketvirtaUzduotis.java
+++ src/ketvirtaUzduotis.java
@@ -1,8 +1,22 @@
+
 public class ketvirtaUzduotis {
     public static void main(String[] args) {
-        String[] zodziai = {"zodziai","kelmas", "botAGAS", "laumė", "mašina"};
+        String[] zodziai = {"zodziai","kelmas", "botAGAS", "laumė","UmUs," "mašina", "burlaivis"};
+        System.out.println(zodziuGaluniuPatikrinimas(zodziai));
     }
-    public static void zodziuGaluniuPatikrinimas(String[] zodziai){
-        boolean teisingiZodziai = false;
+    public static int zodziuGaluniuPatikrinimas(String[] zodziai){
+        int teisingiZodziai = 0;
+        for (String zodis : zodziai) {
+            if (zodis.toLowerCase().contains("as")) {
+                teisingiZodziai++;
+            } else if (zodis.toLowerCase().contains("us")) {
+                teisingiZodziai++;
+            } else if (zodis.toLowerCase().contains("is")) {
+                teisingiZodziai++;
+            }
+            }
+
+        return teisingiZodziai;
     }
 }
+
